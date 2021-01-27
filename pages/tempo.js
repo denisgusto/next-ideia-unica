88dc965ff3cc2905f015fc7739f443ef3243b3d5
Denis Augusto <denisgusto@gmail.com>
2021-01-27 16:14:41 -0300

    Adicionado revalidate

--- pages/tempo.js
+++ pages/tempo.js
@@ -4,22 +4,20 @@
  */
 function Tempo(props) {
     console.log('> Passando pelo Frontend.');
+
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
     return (
         <div>
             <div>{dynamicDateString} (dinâmico)</div>
-            <div>{props.staticDateString} (estático - com delay)</div>
+            <div>{props.staticDateString} (estático)</div>
         </div>
     )
 }
 
 export async function getStaticProps() {
     console.log('> Passando pelo getStaticProps().');
-    console.log('> Adicionado delay de 5 segundos.');
-
-    await delay(5000);
 
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
@@ -27,10 +25,9 @@ export async function getStaticProps() {
     return {
         props: {
             staticDateString
-        }
+        },
+        revalidate: 5 // Quantidade em segundos para revalidar os dados
     }
 }
-
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default Tempo;
