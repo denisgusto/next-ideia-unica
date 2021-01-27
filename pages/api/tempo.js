59a4a538b5af22318ef0c331337ec3711cf5cbe4
Denis Augusto <denisgusto@gmail.com>
2021-01-27 17:16:12 -0300

    Adicionado variável de tempo de cache

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,17 +1,19 @@
 async function tempo(request, response) {
     const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
+    const timeCache = 5; // Tempo em segundos
 
     const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
     const subscribersJson = await subscribersResponse.json();
     const subscribers = subscribersJson.total_subscribers;
 
-    // 10 segundos de cache para as requisições
-    response.setHeader('Cache-Control', 's-maxage=10', 'stale-while-revalidate');
+    // Cache para as requisições
+    response.setHeader('Cache-Control', `s-maxage=${timeCache}`, 'stale-while-revalidate');
 
     response.json({
         date: dynamicDate.toGMTString(),
-        subscribers: subscribers
+        subscribers: subscribers,
+        time_cache: timeCache
     })
 }
 
