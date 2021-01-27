2b62f0ef69b4974187e2c86468777aa2cd108d31
Denis Augusto <denisgusto@gmail.com>
2021-01-27 16:56:06 -0300

    Adicionado API de subscribers da Convertkit

---
+++ pages/api/tempo.js
@@ -0,0 +1,16 @@
+async function tempo(request, response) {
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
+    const dynamicDate = new Date();
+
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+    const subscribersJson = await subscribersResponse.json();
+    const subscribers = subscribersJson.total_subscribers;
+
+
+    response.json({
+        date: dynamicDate.toGMTString(),
+        subscribers: subscribers
+    })
+}
+
+export default tempo;
